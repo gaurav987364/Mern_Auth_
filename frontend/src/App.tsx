d5270642c054c52c8d0a196748b2205517b0e65e
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-04-25 18:35:28 +0530

    fix: remove unnecessary space in main className in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
     <>
       <ToastContainer />
       <Navigation />
-      <main className=" bg-gradient-to-b from-[#fb7185] via-[#a21caf] to-[#6366f1] w-full h-[calc(100vh-70px)] ">
+      <main className="bg-gradient-to-b from-[#fb7185] via-[#a21caf] to-[#6366f1] w-full h-[calc(100vh-70px)] ">
         <Outlet />
       </main>
     </>
