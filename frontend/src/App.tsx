e7cb3c4cdaf1277d1bcb24deb32e3d95d3349b78
gaurav987364 <sharmagourav9654@gmail.com>
2025-09-10 22:45:03 +0530

    ok

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 import Navigation from "./pages/Auth/Navigation";
 import "react-toastify/dist/ReactToastify.css";
-import React from "react";
+import React from "react"
 
 const App:React.FC = () => {
   return (
