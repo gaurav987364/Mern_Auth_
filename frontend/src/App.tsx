3b8f312d909df7a0edf237b19dcf13542e9d9087
gaurav987364 <sharmagourav9654@gmail.com>
2025-09-10 22:46:08 +0530

    again fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import {ToastContainer} from "react-toastify";
+import {ToastContainer} from "react-toastify"
 import Navigation from "./pages/Auth/Navigation"
 import "react-toastify/dist/ReactToastify.css"
 import React from "react"
